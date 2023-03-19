2766c6f97f7105c980f1033cb2503ba43932d913
kgaudium <k.gaudium@gmail.com>
2023-03-20 00:22:10 +0300

    .

---
+++ Quiz/MenuForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
